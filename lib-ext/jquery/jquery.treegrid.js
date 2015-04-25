2025f95351d81849c4c7995922ef08fa4ec6d849
Sandeep Deb <deb.sandeep@gmail.com>
2015-04-26 00:18:12 +0530

    Modified the treegrid lib to store cookies longer

--- lib-ext/jquery/jquery.treegrid.js
+++ lib-ext/jquery/jquery.treegrid.js
@@ -190,7 +190,7 @@
                         stateArray.splice($.inArray(nodeId, stateArray), 1);
                     }
                 }
-                $.cookie($this.treegrid('getSetting', 'saveStateName'), stateArray.join(','));
+                $.cookie($this.treegrid('getSetting', 'saveStateName'), stateArray.join(','), { expires: 30 });
             }
             return $this;
         },
